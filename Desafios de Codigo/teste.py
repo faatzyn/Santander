a434363ec34c70f3c6ed052f5345849f414db206
LuizAssis <=>
2023-10-07 21:46:31 -0300

    Desafio do Power BI

---
+++ Desafios de Codigo/teste.py
@@ -0,0 +1,10 @@
+saldo_atual = float(input())
+valor_deposito = float(input())
+valor_retirada = float(input())
+
+#TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
+saldo_atual = saldo_atual + valor_deposito - valor_retirada
+# saldo_atual = "{:.1f}".format(saldo_atual)
+
+#TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+print(f'Saldo atualizado na conta: {saldo_atual:.1f}')
